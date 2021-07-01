4c8ba898c91fe9780cb3aba3f1144ea83641b2a3
Esteban Porras <estebanporras@Estebans-MBP.fios-router.home>
2021-07-01 13:29:55 -0400

    added scss

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.pl')
+
+panels.forEach((pl) => {
+    pl.addEventListener('click', () => {
+        removeActiveClasses()
+        pl.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(pl => {
+        pl.classList.remove('active')
+    })
+}
